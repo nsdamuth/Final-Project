2cc97983f45f1059bd6385a805490889437701e7
bartstech <bartstech@users.noreply.github.com>
2026-09-12 13:59:19 -0700

    Add Assignment 5 tasks P8.2 and P8.7

---
+++ HodlikBartAssignment5/P8_2/source.cpp
@@ -0,0 +1,95 @@
+/* ---------------------------------------------------------------------------
+** I attest that this following code represents my own work and is subject to
+** the plagiarism policy found in the course syllabus.
+** 
+** Class: 	CSC 242
+** Assignment: 5
+** File: 	source.cpp
+** Description: A program that checks the spelling of all words in a file by
+**              comparing them to a dictionary vector.
+**
+** Author: 	Bart Hodlik, Nick Damuth, Leonard Payne, Sir loverlee Sayoto
+** Date: 	12Sep26
+** -------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+// Function declarations
+void loadDictionary(const string& filename, vector<string>& words);
+bool isWordInDictionary(const string& word, const vector<string>& dictionary);
+string cleanWord(const string& word);
+void checkSpelling(const string& documentFilename, const vector<string>& dictionary);
+
+// Author: Sir loverlee Sayoto
+int main() {
+    vector<string> dictionary;
+    string dictFile = "words.txt";
+    string docFile = "document.txt";
+    
+    loadDictionary(dictFile, dictionary);
+    
+    // Sort dictionary for binary search (in case it is not already sorted)
+    sort(dictionary.begin(), dictionary.end());
+    
+    cout << "Misspelled words in " << docFile << ":" << endl;
+    checkSpelling(docFile, dictionary);
+    
+    return 0;
+}
+
+// Author: Bart Hodlik
+void loadDictionary(const string& filename, vector<string>& words) {
+    ifstream inFile(filename);
+    if (!inFile) {
+        cerr << "Error opening dictionary file: " << filename << endl;
+        return;
+    }
+    string word;
+    while (inFile >> word) {
+        words.push_back(word);
+    }
+    inFile.close();
+}
+
+// Author: Nick Damuth
+bool isWordInDictionary(const string& word, const vector<string>& dictionary) {
+    // Binary search for efficiency
+    return binary_search(dictionary.begin(), dictionary.end(), word);
+}
+
+// Author: Leonard Payne
+string cleanWord(const string& word) {
+    string cleaned = "";
+    for (char c : word) {
+        if (isalpha(c)) {
+            cleaned += tolower(c);
+        }
+    }
+    return cleaned;
+}
+
+// Author: Leonard Payne
+void checkSpelling(const string& documentFilename, const vector<string>& dictionary) {
+    ifstream inFile(documentFilename);
+    if (!inFile) {
+        cerr << "Error opening document file: " << documentFilename << endl;
+        return;
+    }
+    string word;
+    while (inFile >> word) {
+        string cleanedWord = cleanWord(word);
+        if (!cleanedWord.empty()) {
+            if (!isWordInDictionary(cleanedWord, dictionary)) {
+                cout << cleanedWord << endl;
+            }
+        }
+    }
+    inFile.close();
+}
---
+++ HodlikBartAssignment5/P8_7/source.cpp
@@ -0,0 +1,147 @@
+/* ---------------------------------------------------------------------------
+** I attest that this following code represents my own work and is subject to
+** the plagiarism policy found in the course syllabus.
+** 
+** Class: 	CSC 242
+** Assignment: 5
+** File: 	source.cpp
+** Description: Random monoalphabet cipher encryption and decryption.
+**
+** Author: 	Bart Hodlik, Nick Damuth, Leonard Payne, Sir loverlee Sayoto
+** Date: 	12Sep26
+** -------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+// Function declarations
+string generateCipherAlphabet(const string& keyword);
+void encryptFile(const string& inputFile, const string& outputFile, const string& cipherAlphabet);
+void decryptFile(const string& inputFile, const string& outputFile, const string& cipherAlphabet);
+
+// Author: Sir loverlee Sayoto
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        cerr << "Usage: crypt [-d|-e] -kKEYWORD input.txt output.txt" << endl;
+        return 1;
+    }
+
+    bool decrypt = false;
+    string keyword = "";
+    string inputFile = "";
+    string outputFile = "";
+
+    // Parse arguments
+    for (int i = 1; i < argc; ++i) {
+        string arg = argv[i];
+        if (arg == "-d") {
+            decrypt = true;
+        } else if (arg == "-e") {
+            decrypt = false;
+        } else if (arg.substr(0, 2) == "-k") {
+            keyword = arg.substr(2);
+        } else if (inputFile.empty()) {
+            inputFile = arg;
+        } else if (outputFile.empty()) {
+            outputFile = arg;
+        }
+    }
+
+    if (keyword.empty()) {
+        cerr << "Error: Keyword not supplied." << endl;
+        return 1;
+    }
+
+    string cipherAlphabet = generateCipherAlphabet(keyword);
+
+    if (decrypt) {
+        decryptFile(inputFile, outputFile, cipherAlphabet);
+    } else {
+        encryptFile(inputFile, outputFile, cipherAlphabet);
+    }
+
+    cout << "Operation completed successfully." << endl;
+    return 0;
+}
+
+// Author: Bart Hodlik
+string generateCipherAlphabet(const string& keyword) {
+    string cipher = "";
+    bool used[26] = {false};
+
+    // Add unique letters from keyword
+    for (char c : keyword) {
+        if (isalpha(c)) {
+            char upperC = toupper(c);
+            int index = upperC - 'A';
+            if (!used[index]) {
+                cipher += upperC;
+                used[index] = true;
+            }
+        }
+    }
+
+    // Append remaining letters in reverse order
+    for (char c = 'Z'; c >= 'A'; --c) {
+        int index = c - 'A';
+        if (!used[index]) {
+            cipher += c;
+            used[index] = true;
+        }
+    }
+
+    return cipher;
+}
+
+// Author: Nick Damuth
+void encryptFile(const string& inputFile, const string& outputFile, const string& cipherAlphabet) {
+    ifstream in(inputFile);
+    ofstream out(outputFile);
+    if (!in || !out) {
+        cerr << "Error opening files for encryption." << endl;
+        return;
+    }
+    char c;
+    while (in.get(c)) {
+        if (isalpha(c)) {
+            bool isLower = islower(c);
+            char upperC = toupper(c);
+            int index = upperC - 'A';
+            char newChar = cipherAlphabet[index];
+            out << (char)(isLower ? tolower(newChar) : newChar);
+        } else {
+            out << c;
+        }
+    }
+    in.close();
+    out.close();
+}
+
+// Author: Leonard Payne
+void decryptFile(const string& inputFile, const string& outputFile, const string& cipherAlphabet) {
+    ifstream in(inputFile);
+    ofstream out(outputFile);
+    if (!in || !out) {
+        cerr << "Error opening files for decryption." << endl;
+        return;
+    }
+    char c;
+    while (in.get(c)) {
+        if (isalpha(c)) {
+            bool isLower = islower(c);
+            char upperC = toupper(c);
+            int index = cipherAlphabet.find(upperC);
+            char newChar = 'A' + index;
+            out << (char)(isLower ? tolower(newChar) : newChar);
+        } else {
+            out << c;
+        }
+    }
+    in.close();
+    out.close();
+}
